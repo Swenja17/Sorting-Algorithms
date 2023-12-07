08bcd3590c5a629033f019bfe80077357ac31b3e
Swenja17 <112175890+Swenja17@users.noreply.github.com>
2023-12-07 12:19:38 +0000

    .

---
+++ SelectionSort.py
@@ -0,0 +1,9 @@
+from swap import swap
+
+def selection_sort(A):
+    for i in range(len(A)):
+        min_idx = i
+        for j in range(i + 1, len(A)):
+            if A[min_idx] > A[j]:
+                min_idx = j
+        swap(A, i, min_idx)
