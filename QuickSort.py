78226cb0192fc842392679e5a06beaefeaafea35
Swenja17 <112175890+Swenja17@users.noreply.github.com>
2023-12-14 12:04:40 +0000

    hhhhh

--- QuickSort.py
+++ QuickSort.py
@@ -1,1 +1,29 @@
-
+# Sortieralgorithmus
+def quicksort(L, anfang, ende):
+    if L != []:
+        pivot = L[anfang]
+        links = anfang
+        rechts = ende
+        while links <= rechts:
+            while L[links] < pivot:
+                links = links+1
+            while L[rechts] > pivot:
+                rechts = rechts-1
+            if links <= rechts:
+                if links < rechts:
+                    h = L[links]
+                    L[links] = L[rechts]
+                    L[rechts] = h
+                links = links+1
+                rechts = rechts-1
+        if anfang < rechts:
+            L = quicksort(L, anfang, rechts)
+        if links < ende:
+            L = quicksort(L, links, ende)
+    return L
+ 
+# Test
+L = [25, 17, 32, 56, 25, 19, 8, 66, 29, 6, 20, 29]
+print(L)
+L = quicksort(L, 0, len(L)-1)
+print(L)
